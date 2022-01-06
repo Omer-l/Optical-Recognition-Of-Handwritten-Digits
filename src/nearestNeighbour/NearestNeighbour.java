59da5854c05ddc0998e641dbdb3064eeedd407a6
Omer Kacar <omerkacar1@gmail.com>
2022-01-06 10:27:50 +0000

    feat(core): add getDistanceToAllNeighbours()

--- src/nearestNeighbour/NearestNeighbour.java
+++ src/nearestNeighbour/NearestNeighbour.java
@@ -9,6 +9,7 @@ import toolKit.Row; //For assigning each row from the dataset
  * OUTPUT: correct: 2755... incorrect: 55... = 98.04% accuracy
  */
 public class NearestNeighbour extends HandwrittenDigitClassifierAlgorithm {
+    private final int numberOfPossibleClassifications = 10; //digits vary from 0 to 9.
 
     public NearestNeighbour(String algorithmName, Row[] trainingRows, Row[] testRows) {
         super(algorithmName, trainingRows, testRows);
@@ -31,7 +32,6 @@ public class NearestNeighbour extends HandwrittenDigitClassifierAlgorithm {
         }
     }
 
-
     /** classifies new handwritten data by finding closest euclidean distance of the trained data, and then returns the index of that row.
      *
      * @param newRow    the row to classify
@@ -45,6 +45,7 @@ public class NearestNeighbour extends HandwrittenDigitClassifierAlgorithm {
         //gets closest euclidean distance
         for(int rowIndex = 0; rowIndex < trainingRows.length; rowIndex++) {
             Row neighbour = trainingRows[rowIndex];
+
             double distanceToNeighbour = newRow.getEuclideanDistanceTo(neighbour);
 
             //closer than current closest neighbour?
@@ -55,6 +56,38 @@ public class NearestNeighbour extends HandwrittenDigitClassifierAlgorithm {
         }
 
         return trainingRows[closestNeighbourIndex].getClassification();
+    }
+
+    /**
+     * After getting a sorted array of distances to all data points, gets the first 'k'
+     * neighbouring data points and counts their occurrences.
+     * @param k     is the number of desired neighbouring occurences
+     * @return      an array of integers, where each index represents the occurrence of a classification for a neighbour
+     *              (i.e, if the classification is close to where fives' are usually classified, index 4 would ideally have
+     *              the highest count.)
+     */
+    public int[] getKNearestNeighbours(int k) {
+        int[] neighbourCounter = new int[numberOfPossibleClassifications];
+        //sort all training data based on euclidean distance from point.
+
+        return neighbourCounter;
+    }
+
+    /**
+     * Calculates distance to all data points (Rows) in the dataset
+     * @param fromDataPoint     is the row to calculate distance to all other datapoints from
+     * @return                  a sorted array of distances to all data points.
+     */
+    public double[] getDistanceToAllDataPoints(Row fromDataPoint) {
+        Row[] trainingRows = getTrainingRows();
+        double[] distanceToAllDataPoints = new double[trainingRows.length];//distance to all points
+
+        for(int dataPointIndex = 0; dataPointIndex < trainingRows.length; dataPointIndex++) {
+            Row toDataPoint = trainingRows[dataPointIndex];
+            distanceToAllDataPoints[dataPointIndex] = fromDataPoint.getEuclideanDistanceTo(toDataPoint);
+        }
+
+        return distanceToAllDataPoints;
     }
 
 }
---
+++ src/tests/KNearestNeighboursTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import nearestNeighbour.NearestNeighbour;
+import org.junit.Test;
+import toolKit.FileReaderDataset;
+import toolKit.Row;
+
+public class KNearestNeighboursTest {
+    private final static FileReaderDataset TRAINING_FILE_READER = new FileReaderDataset("dataset1.csv"); //for training file reading
+    private final static FileReaderDataset TESTING_FILE_READER = new FileReaderDataset("dataset2.csv"); //for testing file reading
+    private final static Row[] TRAINING_DATA_SETS = TRAINING_FILE_READER.getData(); //training data
+    private final static Row[] TESTING_DATA_SETS = TESTING_FILE_READER.getData(); //test data
+
+
+    @Test
+    public void getDistanceToAllDataPoints() {
+        NearestNeighbour nearestNeighbour = new NearestNeighbour("Nearest Neigbour", TRAINING_DATA_SETS, TESTING_DATA_SETS);
+
+        nearestNeighbour.run();
+    }
+}
